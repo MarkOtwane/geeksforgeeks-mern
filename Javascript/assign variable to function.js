0783a28ce9f6ed1a9fa099898e07ceec699f8e32
MarkOtwane <otwanemark254@gmail.com>
2025-09-10 01:41:49 +0300

    feat: Passing a Function as an Argument to Another Function

--- Javascript/assign variable to function.js
+++ Javascript/assign variable to function.js
@@ -2,3 +2,14 @@ const greetMessage = function () {
 	console.log("Hello, Javascript.");
 };
 greetMessage();
+
+function wrapperFunction() {
+    return "Welcome to GeeksForGeeks!";
+}
+
+function greetMessage(wrapper, name) {
+    const message = wrapper();
+    console.log(`${name}, ${message}`);
+}
+
+greetMessage(wrapperFunction, "Prakash"); // Output: Prakash, Welcome to GeeksForGeeks!

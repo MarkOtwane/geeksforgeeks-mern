59745e039b969b45f8722f41f9dce15cbebd65b3
MarkOtwane <otwanemark254@gmail.com>
2025-10-26 10:29:25 +0300

    Loops in python

---
+++ python/loops/loops.py
@@ -0,0 +1,51 @@
+# a = 5 
+# b =2 
+# if a <= b:
+#     print("A is greater than b")
+# elif a>= b:
+#     print("Good")
+
+# else:
+#     print("A is less than b")
+
+# loops
+#while loop
+# i = 1 
+# while i< 6:
+#     print(i)
+#     if i == 5:
+#         break
+
+    # for loop
+# fruits = ["apples", "Bananas", "Mango", "apples", "Bananas", "Mango"]
+# for x in fruits:
+#     print(x)
+
+# fruits = ["apples", "Bananas", "Mango", "apples", "Bananas", "Mango"]
+# for x in "Bananas":
+#     print(x)
+
+# fruits = ["apples", "Bananas", "Mango", "apples", "Bananas", "Mango"]
+# for x in fruits:
+#     print(x)
+#     if x == "Bananas":
+#         break
+fruits = ["apples", "Bananas", "Mango", "apples", "Bananas", "Mango"]
+# for x in fruits:
+    
+#     if x == "Bananas":
+#         continue
+#     print(x)
+
+# Range
+# for x in range(6):
+#     print(x)
+# Nested loops
+# color = ["Green", "Yellow", "Red"]
+# fruit = ["Mango", "Watermelon", "Apple"]
+# for x in color:
+#     for y in fruit:
+#         print(x, y)
+
+for x in [0, 1, 2]:
+    pass

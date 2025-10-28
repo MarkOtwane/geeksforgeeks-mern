0e7a67a96df97b5d50920ab082d0cca7e0b86d22
MarkOtwane <otwanemark254@gmail.com>
2025-10-29 00:06:12 +0300

    nested dictionaries

--- python/loops/dictionaries.py
+++ python/loops/dictionaries.py
@@ -20,3 +20,42 @@ mydict.update({"year" : 2025})
 mydict["color"] = "red"
 print(mydict)
 
+del mydict["color"]
+print(mydict)
+
+# mydict.clear()
+# print(mydict)
+
+# loops
+# for x in mydict.values():
+#     print(x)
+
+for x in mydict.keys():
+    print(x)
+
+for x, y in mydict.items():
+    print(x, y)
+
+# Nested dictionary
+child1 = {
+    "name":"Mark",
+    "year":2030
+}
+
+child2 = {
+    "name":"Mak",
+    "year":2032
+}
+
+child3 = {
+    "name":"Mrak",
+    "year":2035
+}
+
+fam = {
+    "fam1": child1,
+    "fam2":child2,
+    "fam3": child3
+}
+
+print(fam)

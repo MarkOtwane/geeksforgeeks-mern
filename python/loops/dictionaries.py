89b72f1e37ecdca75849c5c1fc98ea5857183f97
MarkOtwane <otwanemark254@gmail.com>
2025-10-28 13:24:30 +0300

    dictionaries

---
+++ python/loops/dictionaries.py
@@ -0,0 +1,15 @@
+# store data in key and value pair.
+mydict = {
+    "brand":"Ford",
+    "Model":"Mustang",
+    "year": "1998"
+}
+# print(mydict)
+# type casting
+# print(type(mydict["year"]))
+# accessing items
+#get keys method
+x = mydict.keys()
+y = mydict.values()
+z = mydict.items()
+print(z)

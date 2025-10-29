96fc9ed7ab0b9cf58d160e0a1e1ceeb889a07f3d
MarkOtwane <otwanemark254@gmail.com>
2025-10-29 16:28:59 +0300

    args and kwargs

---
+++ python/loops/args.py
@@ -0,0 +1,36 @@
+# Positional Arguments
+
+
+# def numbers(n1, n2):
+#     sum = n1 + n2
+#     return sum
+
+# marks = numbers(12, 32)
+# print("The result of the child is " + str(marks))
+
+# Default Argument
+# def marks(n1 = 20, n2 = 300):
+#     sum = n1 + n2
+#     return sum
+
+# result = marks(6.6)
+# print(result)
+
+#  Key word argument
+# def greetings(name,message):
+#     print("Hello", name)
+#     print(message)
+
+# greetings("Mark", "You're a software engineer")
+
+# *args
+# def funt(*kid):
+#     print("The smallest kid is " + kid[2])
+
+# funt("Mark", "Steve", "Joe", "Moraa")
+
+# kwargs
+def function(child3, child8, child5, child7, child1):
+    print("The cutest child among all is " + child1)
+
+function("Mark", "Steve", "Joe", "Ken","Moraa")

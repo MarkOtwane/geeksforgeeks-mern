51001d68f9a80b02edfcd20743b5ac3714c39cae
MarkOtwane <otwanemark254@gmail.com>
2025-10-31 07:33:38 +0300

    classes in py

--- python/loops/classess.py
+++ python/loops/classess.py
@@ -1,4 +1,13 @@
-# arrays
-cars = ["ford", "volvo", "Marats"]
-x = len(cars)
-print(x)
+# # arrays
+# cars = ["ford", "volvo", "Marats"]
+# x = len(cars)
+# print(x)
+class People:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = People("Mark", 22)
+
+print(p1.name)
+print(p1.age)

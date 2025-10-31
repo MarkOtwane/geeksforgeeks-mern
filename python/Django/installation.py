13918cbbe567a6425c74dab7cce8c4811d05d946
MarkOtwane <otwanemark254@gmail.com>
2025-10-31 08:06:31 +0300

    django

---
+++ python/Django/installation.py
@@ -0,0 +1,12 @@
+# # pip install django
+# # Step 1: Create a virtual environment in your project folder
+# python3 -m venv venv
+
+# # Step 2: Activate it
+# source venv/bin/activate
+
+# # Step 3: Now install Django
+# pip install django
+# deactivate
+# django-admin startproject firstproject
+# python manage.py runserver
